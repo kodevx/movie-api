6464c65bee74a930b11dfa6ea95d949f16fdb84a
kodevx <rohithunni19@yahoo.com>
2020-01-17 12:08:48 +0400

    Initial Setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var DIST_DIR = path(__dirname,'/dist');
+var SRC_DIR = path(__dirname,'/src');
+
+var config = {
+
+    entry: SRC_DIR + '/index.js',
+    output: {
+               path: DIST_DIR + '/src',
+               filename:'bundle.js'        
+    },
+    module:{
+        rules:[ 
+                { test: /\.(js)$/, use:'babel-loader' },
+                { test:/\.(css)$/, use:['style-loader','css-loader'] } 
+        ]
+    },
+    mode: 'development',
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+}
+
+module.exports = config;
