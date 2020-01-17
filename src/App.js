3eb2ae25d412c190db8109efe00d0e76fded1409
kodevx <rohithunni19@yahoo.com>
2020-01-17 14:44:39 +0400

    Added Home Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Home from './component/Home';
 
 const App = () => {
-    return ( <div>Hello World</div> );
+    return ( 
+      <Home /> 
+    );
 }
  
 export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react';
+
+class Home extends Component {
+    state = {  }
+    
+    render() { 
+        console.log("hello!");
+        return ( <div> HomePage! </div>);
+    }
+}
+ 
+export default Home;
