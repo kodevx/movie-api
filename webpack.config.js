db92f29c3d4142b369674b9391445808a4f607e5
kodevx <rohithunni19@yahoo.com>
2020-01-20 13:45:49 +0400

    Webpack and Package Update

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ var config = {
     module:{
         rules:[ 
                 { test: /\.(js)$/, use:'babel-loader' },
-                { test:/\.(css)$/, use:['style-loader','css-loader'] } 
+                { test:/\.(css)$/, use:['style-loader','css-loader'] },
+                { test: /\.(jpe?g|png|gif)$/i, use:[ {loader:'file-loader', options: { outputPath: "assets/"}} ] } 
         ]
     },
     mode: 'development',
